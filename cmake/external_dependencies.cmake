d92aa5cd4ef2eb9f11932733fdee42d8f30eb763
Alexis Placet <alexis.placet@quantstack.net>
2025-12-04 14:56:54 +0100

    try fix

--- cmake/external_dependencies.cmake
+++ cmake/external_dependencies.cmake
@@ -66,6 +66,10 @@ endif()
 
 find_package(Python REQUIRED COMPONENTS Interpreter Development.Module Development.Embed)
 
+execute_process(
+    COMMAND "${Python_EXECUTABLE}" -m nanobind --cmake_dir
+    OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE nanobind_ROOT)
+
 find_package_or_fetch(
     PACKAGE_NAME nanobind
     GIT_REPOSITORY https://github.com/wjakob/nanobind.git
